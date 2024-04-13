b883fe2a4029806556798bb0c5fad0b8714d34e6
saurabh <saurabhroy22@gmail.com>
2024-04-13 15:38:02 +0530

    Scroll on button click

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,18 +1,58 @@
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
+import { useEffect, useState } from "react";
 
 const MovieList=({title, movieType})=>{
+    const [initialMovieList, setInitialMovieList]=useState(null);
+    useEffect(()=>{
+        movieType && setInitialMovieList(movieType.slice(0,9));
 
-    if(!movieType) return;
-    // console.log(movieType);
+    },[movieType])
+    
+    if(!initialMovieList) return;
+       
 
+      const rightClickHandler=()=>{
+          let lastIntialIndex=movieType.indexOf(initialMovieList[initialMovieList.length-1]);
+          let nextIndexForSetInitial=lastIntialIndex+9;
+          if(movieType.length>nextIndexForSetInitial){
+            setInitialMovieList(movieType.slice(lastIntialIndex,nextIndexForSetInitial))
 
-    return (<div >
+          }else{
+            console.log(lastIntialIndex,nextIndexForSetInitial);
+            
+            let newMovieType=[...movieType.slice(-4),...movieType];
+            setInitialMovieList(newMovieType.slice(0,9));
+          }
+
+      }
+      const leftClickHandler=()=>{
+
+        let lastIntialIndex=movieType.indexOf(initialMovieList[initialMovieList.length-1]);
+        let nextIndexForSetInitial=lastIntialIndex+9;
+        if(movieType.length>nextIndexForSetInitial){
+          setInitialMovieList(movieType.slice(lastIntialIndex,nextIndexForSetInitial))
+
+        }else{
+          console.log(lastIntialIndex,nextIndexForSetInitial);
+          
+          let newMovieType=[...movieType.slice(-4),...movieType];
+          setInitialMovieList(newMovieType.slice(0,9));
+        }
+      }
+
+    return (<div  >
+        <div className=" flex justify-between">
          <h1 className=" font-serif text-xl md:text-2xl py-4 px-2">{title}</h1>
+         <div className=" mt-4">
+            <button onClick={leftClickHandler} className=" bg-white  mr-5 w-10 h-10"> <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIkEXE2xHfmLn7D-BfZHJ6MzfX-8zsRvmKd17FoSbX_A&s" alt=" arrow left"/></button>
+            <button onClick={rightClickHandler} className=" bg-white w-10 h-10"> <img src="https://www.svgrepo.com/show/108809/right-arrow.svg" alt=" arrow right"/></button>
+         </div>
+         </div>
       
         <div className=" flex overflow-x-scroll no-scrollbar ">
            <div className=" flex"> {
-                movieType.map(movie=>  <Link to={"/browse/"+ movie.id}  key={movie.id}>  <MovieCard   poster={movie?.poster_path}/> </Link>)
+           initialMovieList.map(movie=>  <Link to={"/browse/"+ movie.id}  key={movie.id}> <MovieCard poster={movie?.poster_path}/> </Link>)
             }
             </div>
         </div>
